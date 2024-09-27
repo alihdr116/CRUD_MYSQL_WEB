bb6f83bbe78778bc610aa949b4124c4131b30a87
alihdr116 <iam.alihdr116@gmail.com>
2024-09-28 03:25:52 +0500

    Update init.sql

--- initdb/init.sql
+++ initdb/init.sql
@@ -1,21 +1,13 @@
--- init.sql
-
--- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS crudmysql;
 
--- Create user 'ali' with password 'root'
 CREATE USER 'ali'@'%' IDENTIFIED BY 'root';
 
--- Grant all privileges on crudmysql database to 'ali'
 GRANT ALL PRIVILEGES ON crudmysql.* TO 'ali'@'%';
 
--- Flush the privileges
 FLUSH PRIVILEGES;
 
--- Use the crudmysql database
 USE crudmysql;
 
--- Create the users table
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
