0ea845740509339019ae2966189d36af06043dbe
alihdr116 <iam.alihdr116@gmail.com>
2024-09-28 03:08:43 +0500

    Update init.sql

--- initdb/init.sql
+++ initdb/init.sql
@@ -17,12 +17,12 @@ USE crudmysql;
 
 -- Create the users table
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,  -- Auto-incrementing ID
-    name VARCHAR(100) NOT NULL,          -- Name field
-    email VARCHAR(100) NOT NULL UNIQUE,  -- Email field
-    age INT NOT NULL,                     -- Age field
-    mobile VARCHAR(15) NOT NULL,         -- Mobile field
-    work VARCHAR(100) NOT NULL,          -- Work field
-    add VARCHAR(255) NOT NULL,           -- Address field
-    `desc` TEXT NOT NULL                 -- Description field
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    age INT NOT NULL,
+    mobile VARCHAR(15) NOT NULL,
+    work VARCHAR(100) NOT NULL,
+    `add` VARCHAR(255) NOT NULL,
+    `desc` TEXT NOT NULL
 );
